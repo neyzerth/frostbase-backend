8f6ff28839ea38ef714e1dd8b2e42d9cf9bf3f84
Neyzer <neyzertoledo@gmail.com>
2025-06-25 20:43:37 -0700

    Delete MS SQL Server in the project

--- FrostBase/Config/Config.cs
+++ FrostBase/Config/Config.cs
@@ -1,6 +1,5 @@
 ﻿public class Config
 {
-    public ConfigSqlServer SqlServer { get; set; }
     public ConfigMongoDB MongoDB { get; set; }
     public ConfigPaths Paths { get; set; }
     public static Config Configuration { get; set; }
--- FrostBase/Config/ConfigSqlServer.cs
+++
@@ -1,9 +0,0 @@
-﻿public class ConfigSqlServer
-{
-    public string Server {  get; set; }
-    public string Port {  get; set; }
-    public string Database { get; set; }
-    public string User {  get; set; }
-    public string Password { get; set; }
-    public string TrustServerCertificate { get; set; }
-}
