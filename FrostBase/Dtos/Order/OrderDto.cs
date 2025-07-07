aae618487bd6e537c9d23e28b45f46bb6498bade
Neyzer <neyzertoledo@gmail.com>
2025-07-07 13:43:29 -0700

    null of DeliveryTime

--- FrostBase/Dtos/Order/OrderDto.cs
+++ FrostBase/Dtos/Order/OrderDto.cs
@@ -2,7 +2,7 @@ public class OrderDto
 {
     public string Id { get; set; }
     public DateOnly Date { get; set; }
-    public DateTime DeliveryTime { get; set; }
+    public DateTime? DeliveryTime { get; set; }
     public UserDto CreatedBy { get; set; }
     public StoreDto Store { get; set; }
 
