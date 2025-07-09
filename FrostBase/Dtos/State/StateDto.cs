d0fcc9345c53ddbffa45564ef75ab95045dae0e8
Neyzer <neyzertoledo@gmail.com>
2025-07-08 22:13:32 -0700

    fix static FromModel

--- FrostBase/Dtos/State/StateDto.cs
+++ FrostBase/Dtos/State/StateDto.cs
@@ -3,7 +3,7 @@ public class StateDto
     public string Id { get; set; }
     public string Description { get; set; }
 
-    public StateDto FromModel(State s)
+    public static StateDto FromModel(State s)
     {
         return new StateDto();
     }
