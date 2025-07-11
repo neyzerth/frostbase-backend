681840e03658c17f2f22a994eb4f089b1beab619
Neyzer <neyzertoledo@gmail.com>
2025-07-10 23:06:24 -0700

    calculate deliver Date of orders

--- FrostBase/Dtos/Order/CreateOrderDto.cs
+++ FrostBase/Dtos/Order/CreateOrderDto.cs
@@ -1,7 +1,6 @@
 public class CreateOrderDto
 {
     public DateTime Date { get; set; }
-    public DateTime DeliverDate { get; set; }
     public string IDUser { get; set; }
     public string IDStore { get; set; }
 }
