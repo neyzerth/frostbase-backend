f106098a50ac3ddd9d40f48a49870a7c88ee626b
Genesis <g.tikyprepa3@gmail.com>
2025-07-16 13:10:22 -0700

    FromModel method created

---
+++ FrostBase/Dtos/Reading/ReadingDto.cs
@@ -0,0 +1,35 @@
+public class ReadingDto
+{
+    public string Id { get; set; }
+    public DateTime Date { get; set; }
+    public bool DoorState {get; set;}
+    public double Temp { get; set; }
+    public int PercHumidity {get; set;}
+    public _Location Location { get; set; }
+    public TruckDto Truck { get; set; }
+    
+    public static ReadingDto FromModel(Reading r)
+    {
+        return new ReadingDto
+        {
+            Id = r.Id,
+            Date = r.Date,
+            DoorState = r.DoorState,
+            Temp = r.Temperature,
+            PercHumidity = r.PercHumidity,
+            Location = new _Location
+            {
+                Latitude = r.Latitude,
+                Longitude = r.Longitude
+            }
+            
+        };
+    }
+}
+
+public class _Location
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
+
--- FrostBase/Dtos/Truck/CreateTruckDto.cs
+++ FrostBase/Dtos/Truck/CreateTruckDto.cs
@@ -2,14 +2,9 @@ using MongoDB.Bson.Serialization.Attributes;
 
 public class CreateTruckDto
 {
-    [BsonId]
     public int Id { get; set; }
-    [BsonElement("brand")]
     public string Brand { get; set; }
-    [BsonElement("model")]
     public string Model { get; set; }
-    [BsonElement("licensePlate")]
     public string LicensePlate { get; set; }
-    [BsonElement("state")]
     public bool State { get; set; }
 }
