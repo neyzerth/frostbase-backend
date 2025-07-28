8283c50d7b08cbaaef5d52ceb354a95445fb6d5c
Neyzer <neyzertoledo@gmail.com>
2025-07-27 19:03:05 -0700

    Add logs

---
+++ FrostBase/Models/TripLog.cs
@@ -0,0 +1,81 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+public class TripLog
+{
+    public static IMongoCollection<TripLog> _tripLogColl = 
+        MongoDbConnection.GetCollection<TripLog>("TripLogs");
+    
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    
+    [BsonElement("date")]
+    public DateTime Date { get; set; }    
+    
+    [BsonElement("IDTrip")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string IDTrip { get; set; }
+    
+    [BsonElement("IDStateTrip")]
+    public string IDStateTrip { get; set; }
+    
+    [BsonElement("orders")]
+    public List<TripOrder> Orders { get; set; }
+
+    public static List<TripLog> Get()
+    {
+        return _tripLogColl.Find(t => true).ToList();      
+    }
+    public static List<TripLog> GetByDate(DateTime date)
+    {
+        return _tripLogColl.Find(t => t.Date == date).ToList();      
+    }
+    public static List<TripLog> GetByTrip(string tripId)
+    {
+        return _tripLogColl.Find(t => t.Id == tripId).ToList();      
+    }
+    public static TripLog Get(string id)
+    {
+        return _tripLogColl.Find(t => t.Id == id).FirstOrDefault();      
+    }
+
+    public static TripLog Insert(TripLog t)
+    {
+        try
+        {
+            t.Id = ObjectId.GenerateNewId().ToString();
+            _tripLogColl.InsertOne(t);
+            return t;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public static TripLog Insert(Trip t, DateTime? date)
+    {
+        try
+        {
+            if(string.IsNullOrEmpty(t.Id))
+                t.Id = ObjectId.GenerateNewId().ToString();
+            var log = new TripLog
+            {
+                Date = date?? DateTime.Now,
+                IDTrip = t.Id,
+                IDStateTrip = t.IDStateTrip,
+                Orders = t.Orders
+            };
+            
+            return Insert(log);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
