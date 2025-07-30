dca8c7d4d35208cfbdf02ee4a193d6b48f4b6e65
Neyzer <neyzertoledo@gmail.com>
2025-07-30 02:10:01 -0700

    Add truck default in user

--- FrostBase/Dtos/User/CreateUserDto.cs
+++ FrostBase/Dtos/User/CreateUserDto.cs
@@ -9,5 +9,6 @@ public class CreateUserDto
     public string Phone { get; set; }
     public DateTime BirthDate { get; set; }
     public string Password { get; set; }
+    public string IDTruckDefault;
     public bool IsAdmin { get; set; }
 }
--- FrostBase/Dtos/User/UserDto.cs
+++ FrostBase/Dtos/User/UserDto.cs
@@ -6,9 +6,14 @@ public class UserDto
     public string Email { get; set; }
     public string Phone { get; set; }
     public DateOnly? BirthDate { get; set; }
+    public TruckDto? TruckDefault { get; set; }
 
     public static UserDto FromModel(UserApp u)
     {
+        TruckDto truck = null;
+        if(string.IsNullOrEmpty(u.IDTruckDefault))
+            truck = TruckDto.FromModel(Truck.Get(u.IDTruckDefault));
+        
         return new UserDto
         {
             Id = u.Id,
@@ -20,6 +25,7 @@ public class UserDto
             },
             Email = u.Email,
             Phone = u.Phone,
+            TruckDefault = truck,
             BirthDate = DateOnly.FromDateTime(u.BirthDate)
         };
     }
