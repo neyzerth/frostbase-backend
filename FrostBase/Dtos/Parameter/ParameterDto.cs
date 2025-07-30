c97f60c4f537f2db4b1bfe85fc3ac8df25591434
Genesis <g.tikyprepa3@gmail.com>
2025-07-30 03:35:07 -0700

    parameterDto, and CreateParameterDto created for update function

---
+++ FrostBase/Dtos/Parameter/ParameterDto.cs
@@ -0,0 +1,23 @@
+public class ParameterDto
+{
+    public decimal MaxTemperature{ get; set; }
+    
+    public int MaxHumidity{ get; set; }
+    
+    public decimal MinTemperature{ get; set; }
+    
+    public int MinHumidity{ get; set; }
+    
+    public static ParameterDto FromModel(Parameter p)
+    {
+        return new ParameterDto
+        {
+            MaxTemperature = p.MaxTemperature,
+            MinTemperature = p.MinTemperature,
+            MaxHumidity = p.MaxHumidity,
+            MinHumidity = p.MinHumidity,
+           
+        };
+    }
+    
+}
--- FrostBase/Models/Parameter.cs
+++ FrostBase/Models/Parameter.cs
@@ -55,5 +55,60 @@ public class Parameter
         }
     }
     
+    public static Parameter Insert(CreateParameterDto p)
+    {
+        Parameter alert = new Parameter
+        {
+            MaxTemperature = p.MaxTemperature,
+            MinTemperature = p.MinTemperature,
+            MaxHumidity = p.MaxHumidity,
+            MinHumidity = p.MinHumidity
+        };
+
+        return Insert(alert);
+    }
+    
+    public static Parameter Update(Parameter updatedParameter)
+    {
+        if (string.IsNullOrEmpty(updatedParameter.Id))
+            throw new ArgumentException("Id cannot be null or empty");
+
+        try
+        {
+            var filter = Builders<Order>.Filter.Eq(p => p.Id, updatedParameter.Id);
+            var update = Builders<Order>.Update
+                .Set(p => p.MaxTemperature, updatedParameter.MaxTemperature)
+                .Set(p => p.MinTemperature, updatedParameter.MinTemperature)
+                .Set(p => p.MaxHumidity, updatedParameter.MaxHumidity)
+                .Set(p => p.MaxHumidity, updatedParameter.MinHumidity);
+            
+            var options = new FindOneAndUpdateOptions<Parameter>
+            {
+                ReturnDocument = ReturnDocument.After // Retorna el documento ya actualizado
+            };
+
+            return _parameterColl.FindOneAndUpdate(filter, update, options);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error updating order: " + e.Message);
+        }
+    }
+
+    public static Parameter Update(UpdateParameterDto updatedParameter)
+    {
+        var param = new Parameter
+        {
+            MaxTemperature = updatedParameter.MaxTemperature,
+            MinTemperature = updatedParameter.MinTemperature,
+            MaxHumidity = updatedParameter.MaxHumidity,
+            MinHumidity = updatedParameter.MinHumidity
+            
+        };
+        return Update(param);
+    }
+}
+    
     #endregion
 }
